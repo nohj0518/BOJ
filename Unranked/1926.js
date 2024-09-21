99742dbf950c7a97c1f1f830267c083a225bbed2
nohj0518 <nohj0518@daum.net>
2024-09-21 17:37:53 +0900

    1926

--- Unranked/1926.js
+++ Unranked/1926.js
@@ -22,16 +22,16 @@ const dx = [-1, 1, 0, 0];
 const dy = [0, 0, -1, 1];
 
 console.log(N, M);
-console.log(graph);
-console.log(visit);
 
-function bfs(x, y) {
+function bfs(x, y, area) {
   let queue = [];
   visit[x][y] += 1;
+  area += 1;
   queue.push([x, y]);
 
   while (queue.length > 0) {
     let [X, Y] = queue.shift();
+    area += 1;
 
     for (let dir = 0; dir < 4; dir++) {
       const nx = X + dx[dir];
@@ -41,17 +41,24 @@ function bfs(x, y) {
       if (visit[nx][ny] > 0 || graph[nx][ny] == 0) continue;
 
       visit[nx][ny] += 1;
-      graph[nx][ny] += graph[x][y];
+      graph[nx][ny] += graph[X][Y];
       queue.push([nx, ny]);
+    }
+  }
+  if (max_area <= area) max_area = area;
+}
+
+let cnt_pic = 0;
+let max_area = 0;
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < M; j++) {
+    if (graph[i][j] == 0 || visit[i][j] > 0) continue;
+    if (graph[i][j] == 1) {
+      cnt_pic += 1;
+      bfs(i, j, 0);
     }
   }
 }
 
-let pictures = 0;
-for (let i = 0; i < N; i++) {
-  for (let j = 0; j < M; j++) {
-    bfs(i, j);
-  }
-}
-
-console.log(graph);
+console.log(cnt_pic);
+console.log(max_area);
